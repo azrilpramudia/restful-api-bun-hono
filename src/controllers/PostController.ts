a36bec91cd3c3cc927c26580afed05c6ef5a1133
azrilpramudia <azrilpramudia01@gmail.com>
2025-08-15 23:50:23 +0700

    finish setup

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -40,3 +40,73 @@ export async function createPost (c: Context) {
         console.error(`Error creating post: ${e}`);
     }
 }
+
+//Getting a post by ID
+export async function getPostById (c: Context) {
+    try {
+        const postId = parseInt(c.req.param('id'));
+        const post = await prisma.post.findUnique({
+            where: { id: postId },
+        });
+
+        if(!post) {
+            return c.json({
+                success: false,
+                message: "Post not found!",
+            }, 404);
+        }
+    
+        return c.json({
+            success: true,
+            message: `Detail Data Post ID : ${postId}`,
+            data: post
+        }, 200);
+    
+    } catch (e: unknown) {
+        console.error(`Error finding post: ${e}`);
+    }
+}
+
+//updating a post
+export async function updatePost (c: Context) {
+    try {
+        const postId = parseInt(c.req.param('id'));
+        const body = await c.req.parseBody();
+        const title = typeof body['title'] === 'string' ? body['title'] : '';
+        const content = typeof body['content'] === 'string' ? body['content'] : '';
+
+        const post = await prisma.post.update({
+            where: { id: postId },
+            data: {
+                title: title,
+                content: content,
+                updatedAt: new Date(),
+            }
+        });
+
+        return c.json({
+            success: true,
+            message: "Post updated successfully!",
+            data: post,
+        }, 200);
+    } catch (e: unknown) {
+        console.error(`Error updating post: ${e}`);
+    }
+}
+
+//Deleting a post
+export async function DeletePost (c: Context) {
+    try {
+        const postId = parseInt(c.req.param('id'));
+        await prisma.post.delete({
+            where: { id: postId },
+        });
+
+        return c.json({
+            success: true,
+            message: "Post deleted successfully!",
+        }, 200);
+    } catch (e: unknown) {
+        console.error(`Error deleting post: ${e}`)
+    }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'hono';
-import { createPost, getPosts } from '../controllers/PostController';
+import { createPost, DeletePost, getPostById, getPosts, updatePost } from '../controllers/PostController';
 
 const router = new Hono();
 
@@ -8,5 +8,14 @@ router.get('/', (c) => getPosts(c));
 
 //routes Posts Create
 router.post('/', (c) => createPost(c));
+
+//router get by id
+router.get('/:id', (c) => getPostById(c));
+
+//router update posts
+router.patch('/:id', (c) => updatePost(c));
+
+//router delete posts
+router.delete('/:id', (c) => DeletePost(c))
 
 export const Routes = router;
